bb69c5e889242ac3c613c52d34f00ee24601949f
fhaque3 <fhaque3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-09-27 04:36:42 +0000

    okay

--- fhaque3/mp2/StickerSheet.cpp
+++ fhaque3/mp2/StickerSheet.cpp
@@ -1,2 +1,23 @@
 #include "StickerSheet.h"
 using namespace cs225;
+StickerSheet::StickerSheet(const Image & picture,unsigned max) {
+//imageloc=picture;
+max_=max;
+}
+StickerSheet::~StickerSheet 	( 		) {}	
+StickerSheet::StickerSheet 	( 	const StickerSheet &  	other	) 	{}
+const StickerSheet& StickerSheet::operator= 	( 	const StickerSheet &  	other	) 	{}
+void StickerSheet::changeMaxStickers 	( 	unsigned  	max	){}
+int StickerSheet::addSticker 	( 	Image &  	sticker,
+		unsigned  	x,
+		unsigned  	y 
+	) 		{}
+bool StickerSheet::translate 	( 	unsigned  	index,
+		unsigned  	x,
+		unsigned  	y 
+	) 	{}
+void StickerSheet::removeSticker 	( 	unsigned  	index	) 	{}
+Image* StickerSheet::getSticker 	( 	unsigned  	index	) 	const {}
+
+Image StickerSheet::render 	( 		) 	const{}
+
