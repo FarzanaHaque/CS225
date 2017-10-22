fc0397d690eda1a49cc9bb91acec96088e484059
fhaque3 <fhaque3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-10-22 19:43:36 +0000

    food

--- fhaque3/lab_avl/avltree.cpp
+++ fhaque3/lab_avl/avltree.cpp
@@ -30,6 +30,18 @@ void AVLTree<K, V>::rotateLeft(Node*& t)
 {
     functionCalls.push_back("rotateLeft"); // Stores the rotation name (don't remove this)
     // your code here
+if(t==NULL) return;
+if(t->right==NULL) return;
+//if(t->right->right==NULL) return;
+Node * z=t;
+Node *y= t->right;
+Node *x=y->right;
+z->right=y->left;
+y->left=z;
+y->right=x;
+t=y;
+z->height=1+max(heightOrNeg1(z->left)+heightOrNeg1(z->right));
+y->height=1+max(heightOrNeg1(y->left)+heightOrNeg1(y->right));
 }
 
 template <class K, class V>
@@ -46,6 +58,20 @@ void AVLTree<K, V>::rotateRight(Node*& t)
 {
     functionCalls.push_back("rotateRight"); // Stores the rotation name (don't remove this)
     // your code here
+if(t==NULL) return;
+if(t->left==NULL)return;
+//if(t->left->left==NULL) return;
+Node *z=t;
+Node *y=t->left;
+Node *x=y->left;
+z->left=y->right;
+y->right=z;
+t=y;
+//implement height changes???
+z->height=1+max(heightOrNeg1(z->left)+heightOrNeg1(z->right));
+y->height=1+max(heightOrNeg1(y->left)+heightOrNeg1(y->right));
+
+
 }
 
 template <class K, class V>
@@ -53,12 +79,33 @@ void AVLTree<K, V>::rotateRightLeft(Node*& t)
 {
     functionCalls.push_back("rotateRightLeft"); // Stores the rotation name (don't remove this)
     // your code here
+rotateRight(t->right);
+rotateLeft(t);
 }
 
 template <class K, class V>
 void AVLTree<K, V>::rebalance(Node*& subtree)
 {
     // your code here
+Node *left=subtree->left;
+Node *right=subtree->right;
+if(height(right)-height(left)<=1 && height(right)-height(left)>=-1)return height(subtree);//balanced
+int hr=height(right);
+int hl=height(left);
+if(hr>hl){//first rotation is left?
+//can't be null because then it couldn't be bigger
+	if(height(right->right)>=height(right->left)){
+		rotateLeft(subtree);
+	}
+	else rotateLeftRight(subtree);
+}
+else{//left is bigger height
+	if(height(left->left)>=height(left->right)){
+		rotateRight(subtree);
+	}
+	else rotateRightLeft(subtree);
+}
+return;
 }
 
 template <class K, class V>
@@ -71,6 +118,17 @@ template <class K, class V>
 void AVLTree<K, V>::insert(Node*& subtree, const K& key, const V& value)
 {
     // your code here
+//idk if i need to redo find but i'm gonna do it anyways
+Node *insertNode=Node(key, value);//do we not need const for the parameters?
+	if(subtree==NULL){
+	subtree=insertNode;
+	return;
+	}
+/*vector<K> inorder=getInorderTraversal(const subtree);
+  for (vector<K>::iterator it = inorder.begin() ; it != inorder.end(); ++it)
+
+}*/
+
 }
 
 template <class K, class V>
