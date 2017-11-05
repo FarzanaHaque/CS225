ddcfb4d231bc85766409025991a2dfeadcd93066
fhaque3 <fhaque3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-11-05 19:23:14 +0000

    first 2

--- mp5/kdtree.cpp
+++ mp5/kdtree.cpp
@@ -9,6 +9,19 @@ bool KDTree<Dim>::smallerDimVal(const Point<Dim>& first,
     /**
      * @todo Implement this function!
      */
+
+double firstD=first[curDim];//(curDim);
+double secondD=second[curDim];//(curDim);
+if(firstD<secondD) return true;
+if(secondD<firstD) return false;
+else
+if(first<second) return true;
+
+
+
+
+
+
     return false;
 }
 
@@ -20,7 +33,23 @@ bool KDTree<Dim>::shouldReplace(const Point<Dim>& target,
     /**
      * @todo Implement this function!
      */
-    return false;
+//return true;
+double pd=0;
+double cbd=0;;
+
+	for(int i=0;i<Dim;i++)
+	{
+	pd=pd+((potential[i]-target[i])*(potential[i]-target[i]));
+	cbd=cbd+((target[i]-currentBest[i])*(target[i]-currentBest[i]));
+	}
+
+if(pd<cbd) return true;
+else if(cbd<pd) return false;
+else if(potential < currentBest) return true;
+else return false;
+
+
+
 }
 
 template <int Dim>
