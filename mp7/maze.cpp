10ed8f9f70522197e89d870d8e969804549e0f4e
fhaque3 <fhaque3@b7fedb43-3d08-4265-b3b9-89e4cbc7bd95>
2017-12-08 03:48:45 +0000

    d

--- mp7/maze.cpp
+++ mp7/maze.cpp
@@ -1,1 +1,19 @@
-/* Your code here! */
+#include <vector>
+#include "dsets.h"
+SquareMaze::SquareMaze(){}
+void SquareMaze::makeMaze (int width,int height) {
+}
+bool canTravel (int x, int y, int dir) const {
+}
+void setWall (int x, int y, int dir, bool exists){
+}
+
+vector< int > SquareMaze::solveMaze (){
+}
+ 
+PNG * SquareMaze::drawMaze () const{
+}
+
+ 
+PNG * SquareMaze::drawMazeWithSolution (){
+}
--- mp7/maze.h
+++ mp7/maze.h
@@ -1,1 +1,32 @@
-/* Your code here! */
+#ifndef _MAZE_H_
+#define _MAZE_H_
+
+#include<vector>
+#include<algorithm>
+#include <iostream>
+
+class SquareMaze {
+	public:
+	SquareMaze()
+	void makeMaze (int width,int height) 
+	bool canTravel (int x, int y, int dir) const 
+	void setWall (int x, int y, int dir, bool exists)
+	vector< int > solveMaze ()
+	 
+	PNG * drawMaze () const
+
+	 
+	PNG * drawMazeWithSolution ()
+
+
+	private:
+
+	//Graph g_;
+	//Vertex startingVertex_;
+
+
+};
+
+#endif
+
+
